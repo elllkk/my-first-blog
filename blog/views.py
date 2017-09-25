8a4afffa1e22fb9a4f9ed08e628230d8347c5250
Elina Kurbangaleeva <elina2241@mail.ru>
2017-09-25 19:33:08 +0500

    dd

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
+from django.http import HttpResponse
+from django import forms
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
+
+
+# Create your views here.
+def upload(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            filehandle = request.FILES['file']
+            return excel.make_response(filehandle.get_sheet(), "csv",
+                                       file_name="download")
+    else:
+        form = UploadFileForm()
+    return render(
+        request,
+        'upload_form.html',
+        {
+            'form': form,
+            'title': 'Excel file upload and download example',
+            'header': ('Please choose any excel file ' +
+                       'from your cloned repository:')
+        })
--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
+                        "django_excel.TemporaryExcelFileUploadHandler")
 
 INSTALLED_APPS = (
     'django.contrib.admin',
